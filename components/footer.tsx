5714f7d54b4d7cad5716fa58b77a0589e1910582
csubj <cj@csubj.io>
2023-03-31 12:10:16 -0700

    fix compile error

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,21 +1,22 @@
-import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
-import Link from 'next/link'
+import Container from "./container";
+import { EXAMPLE_PATH } from "../lib/constants";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="">
       <Container>
         <div className="p-28">
-          <h2>Made with Love on the Internet
-          <Link href="/dino" className="hover:underline">
-          .
-        </Link>
+          <h2>
+            Made with Love on the Internet
+            <Link href="/dino" className="hover:underline">
+              .
+            </Link>
           </h2>
         </div>
       </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- pages/dino.tsx
+++ pages/dino.tsx
@@ -4,7 +4,7 @@ import Container from "../components/container";
 
 function Dino() {
   return (
-        <iframe src="https://chromedino.com/" scrolling="no" width="100%" height="100%" loading="lazy" style={{ 'position': 'absolute', 'width': '100%', 'height': '100%', 'z-index': '999' }}></iframe>
+        <iframe src="https://chromedino.com/" scrolling="no" width="100%" height="100%" loading="lazy" style={{ 'position': 'absolute', 'width': '100%', 'height': '100%' }}></iframe>
   );
 }
 
